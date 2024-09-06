6d724a8765b42a3b001170e875556bdc2204d89f
Vinay@itworks <vinay.poul@infotech.works>
2024-09-06 10:43:13 +0530

    Established a has_one through relationship of User and Bank through Account.

--- app/models/bank.rb
+++ app/models/bank.rb
@@ -1,2 +1,4 @@
 class Bank < ApplicationRecord
+  has_one :account
+  has_one :user, through: :account
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  has_one :account
+  has_one :user, through: :account
 end
